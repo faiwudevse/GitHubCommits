67539aa8319fa16e9eb0137dc03e89089942122f
Fai Wu <fwu@miraclesoft.com>
2019-03-31 14:44:24 -0400

    first commit

---
+++ GitHubCommits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubCommits
+//
+//  Created by Fai Wu on 3/31/19.
+//  Copyright © 2019 Fai Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
